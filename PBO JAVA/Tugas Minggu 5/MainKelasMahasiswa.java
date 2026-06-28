2a5f007065d4f75fa3ddb80a91bcea725251688f
AbiemMayu <abiemayu2210@gmail.com>
2026-06-28 18:50:40 +0700

    Clean up comments in MainKelasMahasiswa.java
    Removed author comments and unnecessary documentation.

---
+++ PBO JAVA/Tugas Minggu 5/MainKelasMahasiswa.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum30032026;
+
+import java.util.Scanner;
+public class MainKelasMahasiswa {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Tugas = ");
+        double tg = input.nextDouble();
+        System.out.print("uts = ");
+        double ut = input.nextDouble();
+        System.out.print("Uas = ");
+        double ua = input.nextDouble();
+        
+        KelasMahasiswa pp1 = new KelasMahasiswa(tg,ut,ua);
+        System.out.println("NilaiAKhir = "+pp1.hitungNilaiAKhir());
+        
+        input.close();
+    }
+    
+    
+}
