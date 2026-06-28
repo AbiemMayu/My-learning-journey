2cc40c67a5b0760112178dff06f3ddcebaed8300
AbiemMayu <abiemayu2210@gmail.com>
2026-06-28 19:12:21 +0700

    Add MainPesawat1 class to demonstrate Pesawat usage

---
+++ PBO JAVA/Tugas minggu 9/MainPesawat1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum27042026;
+
+public class MainPesawat1 {
+    public static void main(String[] args) {
+        // buat objek pada class Pesawat
+        Pesawat p = new Pesawat("Air asia", 2020, true, 130);
+        
+        System.out.println("Kondisi awal");
+        p.cetak();
+        
+        System.out.println();
+        
+        System.out.println("Kondisi setelah reset");
+        p.reset("Lion Air", 1998, false, 80);
+        p.cetak();
+    }
+}
