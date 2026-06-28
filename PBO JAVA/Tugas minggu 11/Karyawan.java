1f36c2341e8ef362175e418ffcc78cd92ae2a049
AbiemMayu <abiemayu2210@gmail.com>
2026-06-28 19:18:02 +0700

    Create Karyawan.java

---
+++ PBO JAVA/Tugas minggu 11/Karyawan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum180526;
+
+public class Karyawan {
+    private String nama;
+
+    public Karyawan(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    
+    public double hitungTotalGaji() {
+        return 0; 
+    }
+
+    
+    public void cetakGaji() {
+        System.out.println("Nama Karyawan : " + this.nama);
+    }
+}
