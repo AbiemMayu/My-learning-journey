078644d99555087290d4a8c301909a110e048877
AbiemMayu <abiemayu2210@gmail.com>
2026-06-28 19:10:39 +0700

    Add Kucing class extending Hewan with sound method

---
+++ PBO JAVA/Tugas minggu 9/Kucing.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum27042026;
+
+public class Kucing extends Hewan {
+    public Kucing(String nama){
+        super(nama);
+    }
+    
+    @Override
+    public void bersuara(){
+        super.bersuara();
+        System.out.println(nama + "\tberkata Meong!!!");
+    }
+}
